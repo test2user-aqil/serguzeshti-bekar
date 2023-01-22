49ef984d9d40283284cafadf3d6b9f6bb1f776dd
Aqil <testuser-aqiL@protonmail.com>
2023-01-22 15:36:56 +0400

    feat: initialize ci

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
 
 const config: UserConfig = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	base: '/serguzeshti-bekar/'
 };
 
 export default config;
