9d5e7ffb67bf73a68c87b799e506b5a8f6accd14
Aqil <testuser-aqiL@protonmail.com>
2023-01-25 06:49:07 +0400

    feat(src): change bendler structure

---
+++ src/routes/bendler/[slug]/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
+<article>
+	<h1>{data.title}</h1>
+	<p>{data.date}</p>
+	<svelte:component this={data.content} />
+</article>
