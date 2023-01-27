1e7b9c0e81e18e01fe2e4093c1b135fe02b6b3aa
Aqil <aqilll083@gmail.com>
2023-01-27 15:12:30 +0400

    fix: remove gh-pages

--- src/routes/bendler/1/+page.md
+++
@@ -1,7 +0,0 @@
----
-title: test
-date: '2023-01-23'
----
-
-test here
-Lorem
--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
 
 const config: UserConfig = {
-	plugins: [sveltekit()],
-	base: '/serguzeshti-bekar/'
+	plugins: [sveltekit()]
 };
 
 export default config;
