448d830c6b09426772ea4415a5b0c79d028090cf
Aqil <aqilll083@gmail.com>
2023-01-27 15:12:23 +0400

    feat(svelte): use sveltepreprocess instead of vite

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,12 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import sveltePreprocess from 'svelte-preprocess'
 const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: sveltePreprocess(),
 
 	kit: {
 		paths: {
