edf1b43f7431c05bd4eba9b959e8325f5cd07000
Aqil <aqilll083@gmail.com>
2023-01-27 15:12:18 +0400

    feat(src): add navbar

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import Navbar from './Navbar.svelte';
+	import './styles.css';
+</script>
+
+<div class="app">
+	<main>
+		<Navbar />
+		<slot />
+	</main>
+</div>
+
+<style>
+	main {
+		padding: 6px;
+	}
+	@media (min-width: 700px) {
+		main {
+			margin: 0 auto;
+			max-width: 700px;
+		}
+	}
+</style>
