70d8dab4dc9abd0a21e0cf077227f6c69b036ccc
Aqil <aqilll083@gmail.com>
2023-01-27 15:12:37 +0400

    feat(src): style improvements

--- src/routes/bendler/+page.svelte
+++ src/routes/bendler/+page.svelte
@@ -8,6 +8,8 @@
 	<title>Bəndlər &bull; Sərgüzəşti-bekar</title>
 </svelte:head>
 
+<h1>Bəndlər</h1>
+
 <ul>
 	{#each data.bendler as bend}
 		<li>
@@ -16,7 +18,32 @@
 					{bend.meta.title}
 				</a>
 			</h2>
-			Published {bend.meta.date}
+			<small>{bend.meta.date}</small>
 		</li>
 	{/each}
 </ul>
+
+<style>
+	h2 {
+		font-size: 32px;
+	}
+
+	a {
+		color: #101010;
+	}
+
+	ul {
+		display: flex;
+		flex-direction: column;
+		gap: 1rem;
+	}
+
+	li {
+		padding: 1rem 2rem;
+		border-radius: 1rem;
+		background-color: var(--accent-2);
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+	}
+</style>
---
+++ src/routes/bendler/test-post.md
@@ -0,0 +1,6 @@
+---
+title: Test post
+date: '2023-01-26'
+---
+
+Test post here lmaslasdmasdna( random )
