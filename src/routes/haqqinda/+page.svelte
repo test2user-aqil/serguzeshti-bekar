daf8e7bffca4d21fa1167a9f909b296e85e52b15
Aqil <aqilll083@gmail.com>
2023-01-27 15:12:49 +0400

    feat(src): update `/haqqinda` Fill page with some data

--- src/routes/haqqinda/+page.svelte
+++ src/routes/haqqinda/+page.svelte
@@ -3,3 +3,23 @@
 </svelte:head>
 
 <h1>Haqqında</h1>
+
+<h2>Təvir</h2>
+<div>Bekarların məcəralarının olduğu veb-səhifə</div>
+
+<h2>Linklər</h2>
+
+<div>
+	<ul>
+		<a href="https://github.com/test2user-aqil/serguzeshti-bekar">GitHub</a>
+	</ul>
+</div>
+
+<style>
+	h2 {
+		margin: 0.8rem 0;
+	}
+	div {
+		margin: 0 0 3rem;
+	}
+</style>
