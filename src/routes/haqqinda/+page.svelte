a3333182d437e64497326eb8fa5b0e911e9ca9e7
Aqil <aqilll083@gmail.com>
2023-01-28 12:41:21 +0400

    chore(haqqinda): update Add authors & improve style

--- src/routes/haqqinda/+page.svelte
+++ src/routes/haqqinda/+page.svelte
@@ -7,19 +7,32 @@
 <h2>Təvir</h2>
 <div>Bekarların məcəralarının olduğu veb-səhifə</div>
 
-<h2>Linklər</h2>
+<h2>Müəlliflər</h2>
+<ul>
+	<li>Qədirli Sübhan</li>
+	<li>Məmmədov Aqil</li>
+</ul>
 
-<div>
-	<ul>
-		<a href="https://github.com/test2user-aqil/serguzeshti-bekar">GitHub</a>
-	</ul>
-</div>
+<h2>Linklər</h2>
+<ul>
+	<li>
+		<a href="https://github.com/test2user-aqil/serguzeshti-bekar" target="_blank" rel="noreferrer">
+			GitHub
+		</a>
+	</li>
+</ul>
 
 <style>
 	h2 {
-		margin: 0.8rem 0;
+		margin: 3rem 0 1rem;
 	}
 	div {
 		margin: 0 0 3rem;
+	}
+	ul {
+		list-style-type: disc;
+	}
+	li {
+		margin: 0.8rem 0.4rem;
 	}
 </style>
