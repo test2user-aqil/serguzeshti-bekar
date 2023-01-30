bc6c7fa7147a050d972640d22d16d92db9537f2a
Aqil <aqilll083@gmail.com>
2023-01-30 19:59:51 +0400

    chore(src): update homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,6 +15,11 @@
 <div class="container">
 	<h1>Sərgüzəşti-bekar</h1>
 
+	<h3>
+		Bəndlərin heç biri təbii və ya süni bir təfəkkürün məhsulu deyil. Real hadisələr əsasında
+		yazılmışdır.
+	</h3>
+
 	<h2>Ən son:</h2>
 	<Bend path={data.bend.path} title={data.bend.meta.title} date={data.bend.meta.date} />
 </div>
@@ -26,8 +31,13 @@
 		align-items: stretch;
 		gap: 1rem;
 	}
-	h2 {
+	h2,
+	h3 {
 		font-family: 'Merriweather', serif;
 		margin: 0 1rem;
+	}
+	h3 {
+		text-align: center;
+		margin: 2rem 0 1rem 0;
 	}
 </style>
